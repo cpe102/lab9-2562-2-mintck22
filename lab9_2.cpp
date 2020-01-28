d74098c2cac6b00d11f59769a6f747c5ab77a037
Mint <chawanluck_k@hotmail.com>
2020-01-28 16:40:14 +0700

    2

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,3 +2,32 @@
 //cout << textline;
 //cout << "-------------------- SOTUS ---------------------";
 //Don't forget to change cout to output filestream
+#include<iostream>
+#include<string>
+#include<fstream>
+using namespace std;
+
+int main(){
+
+    string textline;
+    
+
+    ifstream source1;
+    source1.open("cheerbook.txt");
+    
+
+    ofstream dest;
+    dest.open("cheerbook_copy.txt");
+    dest << "-------------------- SOTUS ---------------------\n";
+    
+    while (getline(source1,textline)){
+        
+        dest << textline << "\n";
+        
+    }
+    dest << "-------------------- SOTUS ---------------------";
+    dest.close();
+
+    return 0;
+
+}
